bffc3e1b63e75c7c7ff1f028bf782bdf38a073c2
Laurine Maina <mainalaureen58@gmail.com>
2024-03-22 17:44:41 +0300

    initial commit

--- solution1.js
+++ solution1.js
@@ -1,21 +1,17 @@
-// Write a program that prompts the user to input student marks. The input should be between 0 and 100. 
-// Then output the correct grade: 
-// A > 79, B - 60 to 79, C -  49 to 59, D - 40 to 49, E - less 40.
-
 function studGrade(input){
     if (input>=100){
-        return "Invalid" ;}
+        return "Invalid" ;} // does not give out a grade because the input is more than 100
     
     if (input>=79){
-        return "A"
+        return "A";
     }else if(input<79 && input>=60){
-        return "B"
+        return "B";
     }else if(input<=59 && input>=49){
-        return "C"
+        return "C";
     }else if(input<=49 && input>=40){
-        return "D"
+        return "D";
     }else{
-        return "E"
+        return "E";
     }
 }
-studGrade()
+studGrade() // where you call out your function with an argument(input)
