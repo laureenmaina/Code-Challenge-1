b75a8187c740680687fca331771c90db6eb40db8
Laurine Maina <mainalaureen58@gmail.com>
2024-03-23 16:11:43 +0300

    initial commit

--- solution2.js
+++ solution2.js
@@ -1,19 +1,13 @@
-// Write a program that takes as input the speed of a car e.g 80. If the speed is less than 70,
-//  it should print “Ok”. Otherwise, for every 5 km/s above the speed limit (70), 
-// it should give the driver one demerit point and print the total number of demerit points.
-// For example, if the speed is 80, it should print: “Points: 2”. If the driver gets more than 12 points,
-//  the function should print: “License suspended”.
-
-const speedLimit=70
+const speedLimit=70  // speed Limit is 70km/s
 function speedRules(speedLimit,input){
     if(input<=speedLimit){
-        return "Ok"
+        return "Ok"         // if speed is 70 km/s or less,it returns 'Ok'
     }
-    const demeritPoints=((input-speedLimit)/5)
+    const demeritPoints=((input-speedLimit)/5) // calculates every 5 km/s above the speed limit (70)
     if(demeritPoints>12){
-        return "Licence suspended"}
+        return "Licence suspended"} // returns license suspended if the demerits are above 12
     else{
-        return "Demerit Points:"+ [demeritPoints];
+        return "Demerit Points:"+ [demeritPoints]; //returns the number of demerits a driver has if he exceeded the speed linit
     }
-}console.log(speedRules(70,200))
+}console.log(speedRules(70,200)) // enter the value of speedLimit and input
 
